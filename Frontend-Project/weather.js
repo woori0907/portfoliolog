f24675e92eb96ee3b5f8cb697a7e72d2a09423bb
Woori Kang <rose5307@naver.com>
2021-04-01 23:41:39 +0900

    날씨정보 추가. api를 통해 데이터를 받아와서 적용하는 부분까지 배움!

--- Frontend-Project/weather.js
+++ Frontend-Project/weather.js
@@ -1,4 +1,17 @@
-const COORDS = 'coords';
+const API_KEY = 'e624cfe13d5384545872f83e1779948d';
+const COORDS = 'coords'; 
+const weather = document.querySelector('.js-weather');
+
+function getWeather(lat, lon){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`).then(function(response){
+        return response.json();
+    }).then(function(json){
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`;
+    });
+    // then : 외부 api참조시 데이터가 들어오는데 시간이 걸릴 수 있음. then은 이러한 경우 이전 함수가 완료된 이후에 실행되도록 함. 만약 이것을 사용하지 않고 바로 함수를 실행시켰을 경우 이전 작업이 완료되지 않은 상태에서 다음 문장이 실행되어 오류가 발생할 수 있음.
+}
 
 function saveCoords(coordsObj){
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -13,6 +26,7 @@ function handleGeoSuccess(position){
     };
     // 오브젝트 선언시 key와 값의 이름이 같을 경우 하나만 적어도 됨.
     saveCoords(coordObj);
+    getWeather(latitude, longitude);
 }
 
 function handleGeoError(){
@@ -30,8 +44,9 @@ function loadCoords(){
         askForCoords();
     }
     else{
-
-    }
+        const parseCoords = JSON.parse(loadedCords);
+        getWeather(parseCoords.latitude, parseCoords.longitude);
+    }   
 }
 
 function init(){
