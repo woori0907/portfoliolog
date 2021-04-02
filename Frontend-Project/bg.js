83f18190a92012aec1f6ba0e79ee2c07e67474fd
Woori Kang <rose5307@naver.com>
2021-04-02 23:43:22 +0900

    언스플래시 api 추가

--- Frontend-Project/bg.js
+++ Frontend-Project/bg.js
@@ -5,7 +5,7 @@ const IMG_NUMBER = 3;
 
 function paintImage(imgNumber){
     const image = new Image();
-    image.src = `images/${imgNumber + 1}.jpg`;
+    image.src = `https://source.unsplash.com/random?landscape?1920x1080`;
     image.classList.add("bgImage");
     body.appendChild(image);
 }
