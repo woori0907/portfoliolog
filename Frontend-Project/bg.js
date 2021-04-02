d8444c6fce801c38345f0361778030804d1d80fe
Woori Kang <rose5307@naver.com>
2021-04-03 01:08:46 +0900

    배경화면 형식 변경. appendChild 대신 backgroundImage로 추가함.

--- Frontend-Project/bg.js
+++ Frontend-Project/bg.js
@@ -1,13 +1,15 @@
-const body = document.querySelector("body");
+const backGround = document.querySelector(".js-backGround");
 
 const IMG_NUMBER = 3;
 
 
 function paintImage(imgNumber){
     const image = new Image();
-    image.src = `https://source.unsplash.com/random?landscape?1920x1080`;
+    image.src = `https://source.unsplash.com/1600x900/?landscape`;
     image.classList.add("bgImage");
-    body.appendChild(image);
+    backGround.style.backgroundImage = `url('${image.src}')`;
+    backGround.style.backgroundPosition = 'center center';
+    backGround.style.backgroundSize = 'cover';
 }
 
 function genNumber(){
